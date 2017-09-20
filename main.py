b57119be5201b8926727ec55e4ef054a4983ebe3
dmytroPustovit <pustic@gmail.com>
2017-09-21 00:36:16 +0300

    set_year

--- main.py
+++ main.py
@@ -8,39 +8,43 @@ from datetime import date
 
 class Page():
 	__metaclass__ = ABCMeta
-	def __init__(self, name, result='out' dir_source="source/", dir_results="results/"):
+	def __init__(self, name, result='out', dir_source="source/", dir_results="results/"):
 		self.dir_source = dir_source
 		self._prepare_folder(self.dir_source)
 		self.link = dir_source + name
 		self.name = result
+		self.now = date.today()
 
 	def read(self,format='csv'):
 		if format=='csv':
 			pass
 
-	def save(self,type='csv',name=self.name):
+	def save(self,type='csv'):
+		name=save.name
 		if type=='json':
 			self._save_json(self.dir_results+name,elements)
 
 	# protected level
+	def _set_year(self,month,day):
+		if self.now>date(self.now.year,month,day):
+			return self.now.year
+		return self.now.year+1
+
 	def _read_csv(self):
-		with open(self.link) as csvfile:
-			reader = csv.DictReader(csvfile,delimiter =";")
-			for row in reader:
-				print(row['name'],row['date'])
+		pass
 	def _write_csv(self):
 		with open(self, 'w',newline='') as csvfile:
-		fieldnames = ['Subject','Start Date','All Day Event','Private']
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter =",")
-		writer.writeheader()
-		for event in self.data:
-			record = {
-			'Subject': event['name']+"'s birthday",
-			'Start Date': event['date']
-			'All Day Event': True
-			'Private': True
-			}
-			writer.writerow(record)
+			fieldnames = ['Subject','Start Date','All Day Event','Private']
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter =",")
+			writer.writeheader()
+			for event in self.data:
+				record = {
+				'Subject': event['name']+"'s birthday",
+				'Start Date': event['date'],
+				'All Day Event': True,
+				'Private': True
+				}
+				writer.writerow(record)
 	
 	def _save_json(self,name,elements):
 		jsonString = JSONEncoder(ensure_ascii = False).encode(elements)

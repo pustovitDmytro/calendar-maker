bd719cee135ed282b162fd6ce4877e64c283fc89
dmytroPustovit <pustic@gmail.com>
2017-09-23 21:26:20 +0300

    csv completed

--- main.py
+++ main.py
@@ -11,50 +11,63 @@ class Page():
 	def __init__(self, name, result='out', dir_source="source/", dir_results="results/"):
 		self.dir_source = dir_source
 		self._prepare_folder(self.dir_source)
+		self._prepare_folder(dir_results);
 		self.link = dir_source + name
-		self.name = result
+		self.name = dir_results+result
 		self.now = date.today()
 
 	def read(self,format='csv'):
 		if format=='csv':
-			pass
+			self._read_csv();
 
 	def save(self,type='csv'):
-		name=save.name
+		name=self.name
 		if type=='json':
-			self._save_json(self.dir_results+name,elements)
+			self._save_json(self.dir_results+name, self.elements)
+		elif type=='csv':
+			self._write_csv()
+		elif type=='ics':
+			self._write_ics()
 
 	# protected level
 	def _set_year(self,month,day):
-		if self.now>date(self.now.year,month,day):
+		if self.now<date(self.now.year,month,day):
 			return self.now.year
 		return self.now.year+1
+
 	def get_date(self,strdate):
-		date =  datetime.strptime(strdate,'%d.%m');
-		print(date);
-		return date;
+		odate =  datetime.strptime(strdate,'%d.%m')
+		year = self._set_year(odate.month,odate.day)
+		return date(year,odate.month,odate.day)
+
 	def _read_csv(self):
 		with open(self.link) as csvfile:
 			reader = csv.DictReader(csvfile,delimiter =";")
 			self.events = []
 			for row in reader:
-				print(row);
 				record = {
-				'name': row['name']+"'s birthday",
+				'name': row['name'],
 				'date': self.get_date(row['date'])
 				}
 				self.events.append(record)
-		print(self.events)
-
+	def _write_ics(self):
+		with open(self.name+'.ics','w') as file:
+			file.write("BEGIN:VCALENDAR\n")
+			file.write("VERSION:2.0\n")
+			file.write("PRODID:1\n")
+			for event in self.events:
+				file.write("BEGIN:VEVENT\n")
+				file.write("END:VEVENT\n")
+			file.write("END:VCALENDAR\n")
 	def _write_csv(self):
-		with open(self, 'w') as csvfile:
+		with open(self.name+'.cvs', 'w') as csvfile:
 			fieldnames = ['Subject','Start Date','All Day Event','Private']
-			writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter =",",newline='')
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter =",")
 			writer.writeheader()
-			for event in self.data:
+			for event in self.events:
 				record = {
 				'Subject': event['name']+"'s birthday",
-				'Start Date': event['date'],
+				'Start Date': event['date'].strftime('%d/%m/%Y'),
 				'All Day Event': True,
 				'Private': True
 				}
@@ -70,4 +83,5 @@ class Page():
 			os.makedirs(path)
 
 page = Page('family.txt')
-page._read_csv();
+page.read();
+page.save(type='ics');
